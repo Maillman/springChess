4876f52891a09bd1c087aac450dee447cdd25339
Melvin Whitaker <awsome7mailman@gmail.com>
2025-05-09 09:00:18 -0600

    Added spark endpoints

--- server/src/main/java/server/Handler.java
+++ server/src/main/java/server/Handler.java
@@ -1,15 +1,71 @@
 package server;
 
+import com.google.gson.Gson;
+
+import dataaccess.AuthDAO;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import dataaccess.memory.MemoryAuthDAO;
+import dataaccess.memory.MemoryGameDAO;
+import dataaccess.memory.MemoryUserDAO;
+import model.UserData;
 import service.ClearService;
 import service.GameService;
 import service.UserService;
+import spark.Request;
+import spark.Response;
 
 public class Handler {
-    private UserService userService;
-    private GameService gameService;
-    private ClearService clearService;
+    private final UserService userService;
+    private final GameService gameService;
+    private final ClearService clearService;
 
     public Handler() {
-        
+        UserDAO userDAO = new MemoryUserDAO();
+        AuthDAO authDAO = new MemoryAuthDAO();
+        GameDAO gameDAO = new MemoryGameDAO();
+        this.userService = new UserService(userDAO, authDAO);
+        this.gameService = new GameService(userDAO, authDAO, gameDAO);
+        this.clearService = new ClearService(userDAO, authDAO, gameDAO);
+    }
+
+    public Object register(Request req, Response res) {
+        UserData user = new Gson().fromJson(req.body(), UserData.class);
+    }
+
+    public Object login(Request req, Response res) {
+        Object placeholder = "Remove this line";
+
+        return placeholder;
+    }
+
+    public Object logout(Request req, Response res) {
+        Object placeholder = "Remove this line";
+
+        return placeholder;
+    }
+
+    public Object listGames(Request req, Response res) {
+        Object placeholder = "Remove this line";
+
+        return placeholder;
+    }
+
+    public Object createGame(Request req, Response res) {
+        Object placeholder = "Remove this line";
+
+        return placeholder;
+    }
+
+    public Object joinGame(Request req, Response res) {
+        Object placeholder = "Remove this line";
+
+        return placeholder;
+    }
+
+    public Object clear(Request req, Response res) {
+        Object placeholder = "Remove this line";
+
+        return placeholder;
     }
 }
--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -15,6 +15,13 @@ public class Server {
         Spark.staticFiles.location("web");
 
         // Register your endpoints and handle exceptions here.
+        Spark.post("/user", (req, res) -> handler.register(req, res));
+        Spark.post("/session", (req, res) -> handler.login(req, res));
+        Spark.delete("/session", (req, res) -> handler.logout(req, res));
+        Spark.get("/game", (req, res) -> handler.listGames(req, res));
+        Spark.post("/game", (req, res) -> handler.createGame(req, res));
+        Spark.put("/game", (req, res) -> handler.joinGame(req, res));
+        Spark.delete("/db", (req, res) -> handler.clear(req, res));
 
         //This line initializes the server and can be removed once you have a functioning endpoint 
         Spark.init();
