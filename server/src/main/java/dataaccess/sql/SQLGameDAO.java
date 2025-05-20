252531a142872ad8bddf90ceea3018652d28155c
Melvin Whitaker <awsome7mailman@gmail.com>
2025-05-20 15:17:00 -0600

    Added getGame for SQLGameDAO

--- server/src/main/java/dataaccess/sql/SQLGameDAO.java
+++ server/src/main/java/dataaccess/sql/SQLGameDAO.java
@@ -1,5 +1,10 @@
 package dataaccess.sql;
 
+import static java.sql.Statement.RETURN_GENERATED_KEYS;
+
+import com.google.gson.Gson;
+
+import chess.ChessGame;
 import dataaccess.DataAccessException;
 import dataaccess.DatabaseManager;
 import dataaccess.GameDAO;
@@ -10,7 +15,27 @@ public class SQLGameDAO implements GameDAO {
 
     @Override
     public GameData getGame(int gameID) throws DataAccessException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "SELECT * FROM games WHERE game_id=?";
+            try (var ps = conn.prepareStatement(statement)) {
+                ps.setInt(1, gameID);
+                try (var rs = ps.executeQuery()) {
+                    if(rs.next()) {
+                        String whiteUsername = rs.getString("white_username");
+                        String blackUsername = rs.getString("black_username");
+                        String gameName = rs.getString("game_name");
+                        String chessGameJSON = rs.getString("game");
+                        ChessGame game = new Gson().fromJson(chessGameJSON, ChessGame.class);
+                        GameData foundGame = new GameData(gameID, whiteUsername, blackUsername, gameName, game);
+                        return foundGame;
+                    }
+                }
+            }
+        } catch(Exception ex) {
+            System.out.println(ex);
+            throw new DataAccessException("Error getting user: " + ex.getMessage(), 500);
+        }
+        return null;
     }
 
     @Override
@@ -20,7 +45,26 @@ public class SQLGameDAO implements GameDAO {
 
     @Override
     public int createGame(String gameName) throws DataAccessException {
-        throw new UnsupportedOperationException("Not supported yet.");
+        try (var conn = DatabaseManager.getConnection()) {
+            var statement = "INSERT INTO games (white_username, black_username, game_name, game) VALUES (?, ?, ?)";
+            try (var ps = conn.prepareStatement(statement, RETURN_GENERATED_KEYS)) {
+                ps.setString(1, null);
+                ps.setString(2, null);
+                ps.setString(3, gameName);
+                String jsonGame = new Gson().toJson(new ChessGame());
+                ps.setString(4, jsonGame);
+                ps.executeUpdate();
+                
+                var rs = ps.getGeneratedKeys();
+                if (rs.next()) {
+                    return rs.getInt(1);
+                }
+                return -1;
+            }
+        } catch (Exception ex) {
+            System.out.println(ex);
+            throw new DataAccessException("Error creating user: " + ex.getMessage(), 500);
+        }
     }
 
     @Override
