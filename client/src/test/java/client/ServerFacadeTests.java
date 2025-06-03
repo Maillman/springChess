bd9ed7cf7c878ef5db01e4bc74d9cefb6ef7be6e
Melvin Whitaker <awsome7mailman@gmail.com>
2025-06-03 16:15:40 -0600

    Added Register ServerFacade Test

--- client/src/test/java/client/ServerFacadeTests.java
+++ client/src/test/java/client/ServerFacadeTests.java
@@ -5,19 +5,25 @@ import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import model.AuthData;
+import model.UserData;
+import network.ServerFacade;
 import server.Server;
 
 
 public class ServerFacadeTests {
 
     private static Server server;
-    private static ServerFacade serverFacade;
+    static ServerFacade serverFacade;
+    private static final UserData SAMPLE_USER = new UserData("newUser","newPass","new@email.com");
 
     @BeforeAll
     public static void init() {
         server = new Server();
         var port = server.run(0);
         System.out.println("Started test HTTP server on " + port);
+        String url = "http://localhost:" + port;
+        serverFacade = new ServerFacade(url);
     }
 
     @AfterAll
@@ -27,8 +33,12 @@ public class ServerFacadeTests {
 
 
     @Test
-    public void registerSuccess() {
-        Assertions.assertTrue(true);
+    public void registerSuccess() throws Exception {
+        AuthData registerResponse = serverFacade.register(SAMPLE_USER);
+        System.out.println(registerResponse);
+        Assertions.assertNotNull(registerResponse, "Register Response is null");
+        Assertions.assertNotNull(registerResponse.username(), "Username is null");
+        Assertions.assertNotNull(registerResponse.authToken(), "AuthToken is null");
     }
 
 }
